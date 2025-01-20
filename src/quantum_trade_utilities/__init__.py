3df092b0b81cf6534228945b9de5af343877a2b6
japoeder <japoeder@gmail.com>
2025-01-20 16:38:16 -0600

    rework airflow items and remove numpy and pandas requirements.

--- src/quantum_trade_utilities/__init__.py
+++ src/quantum_trade_utilities/__init__.py
@@ -10,15 +10,14 @@ from quantum_trade_utilities.data.load_credentials import load_credentials
 from quantum_trade_utilities.data.request_url_constructor import request_url_constructor
 
 from quantum_trade_utilities.analysis.backtest_summary import backtest_summary
-from quantum_trade_utilities.analysis.trade_data import trade_data
 from quantum_trade_utilities.analysis.finbert_utils import estimate_sentiment
-
+from quantum_trade_utilities.analysis.exch_list import pull_exch_list
 from quantum_trade_utilities.io.grab_html import grab_html
 from quantum_trade_utilities.io.delete_logs import delete_logs
 from quantum_trade_utilities.io.logging_config import setup_logging
 
 from quantum_trade_utilities.time.std_article_time import std_article_time
-
+from quantum_trade_utilities.time.is_trading_time import is_trading_time
 from quantum_trade_utilities.debug.debug_util import bp
 
 __version__ = "0.1.0"
@@ -33,11 +32,11 @@ __all__ = [
     "load_credentials", "request_url_constructor",
     
     # Analysis
-    "backtest_summary", "trade_data", "estimate_sentiment",
+    "backtest_summary", "estimate_sentiment", "pull_exch_list",
     
     # IO
     "grab_html", "delete_logs", "setup_logging",
     
     # Time
-    "std_article_time",
+    "std_article_time", "is_trading_time",
 ]
