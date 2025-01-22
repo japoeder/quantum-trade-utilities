0207eba6e3f60f2d99ccdc76a6e5a996b513654e
japoeder <japoeder@gmail.com>
2025-01-21 22:32:20 -0600

    update load credentials for bachman, groq, and qdrant.

--- src/quantum_trade_utilities/data/load_credentials.py
+++ src/quantum_trade_utilities/data/load_credentials.py
@@ -122,6 +122,39 @@ def load_credentials(file_path, data_type):
             reddit_creds["REDDIT_USER"],
             reddit_creds["REDDIT_PWD"],
         )
+    
+    if data_type == "qdrant_ds":
+        with open(file_path, "r", encoding="utf-8") as file:
+            creds = json.load(file)
+        qdrant_creds = creds["qdrant_ds"]
+        return (
+            qdrant_creds["QDRANT_HOST"],
+            qdrant_creds["QDRANT_PORT"],
+        )
+
+    if data_type == "groq_api":
+        with open(file_path, "r", encoding="utf-8") as file:
+            creds = json.load(file)
+        groq_creds = creds["groq_api"]
+        return (
+            groq_creds["API_KEY"]
+        )
+    
+    if data_type == "huggingface_api":
+        with open(file_path, "r", encoding="utf-8") as file:
+            creds = json.load(file)
+        huggingface_creds = creds["huggingface_api"]
+        return (
+            huggingface_creds["API_KEY"],
+        )
+
+    if data_type == "bachman_api":
+        with open(file_path, "r", encoding="utf-8") as file:
+            creds = json.load(file)
+        bachman_creds = creds["bachman_api"]
+        return (
+            bachman_creds["API_KEY"],
+        )
 
     else:
         raise ValueError(f"Invalid data type: {data_type}")
