2c7298c45e4544ec53b802d4e7c3dd89b049d6e3
japoeder <japoeder@gmail.com>
2025-02-05 01:36:36 -0600

    add rotation of xaxis labels darkviz.

--- src/quantum_trade_utilities/viz/darkviz.py
+++ src/quantum_trade_utilities/viz/darkviz.py
@@ -18,7 +18,9 @@ dark_style = {
     'axes.labelcolor': '0.9',
     'xtick.color': '0.9',
     'ytick.color': '0.9',
-    'font.size': 12
+    'font.size': 12,
+    'xtick.rotation': 45,
+    'ytick.rotation': 0,
 }
 plt.rcParams.update(dark_style)
 print(f'Preferred darkviz plot colors: "fuchsia" and "lime"')
